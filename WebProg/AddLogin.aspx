d7efb81f9761d3a5c4d325c0b368a39c223b1752
Sean Golden <seangol1@hotmail.com>
2017-07-20 17:35:32 +0100

    add a login

--- WebProg/AddLogin.aspx
+++ WebProg/AddLogin.aspx
@@ -8,11 +8,35 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div class="row">
             <h2>Add A Login</h2>
-            <asp:Label ID="Label1" runat="server" Text="Website:">
 
+            <div id="websiteDetails" class="col-md-4">
+            <asp:Label ID="Label1" runat="server" Text="Website:">
+                <asp:TextBox ID="websiteName" runat="server"></asp:TextBox>
             </asp:Label>
+            <asp:Label ID="Label2" runat="server" Text="Website Address:">
+                <asp:TextBox ID="link" runat="server"></asp:TextBox>
+            </asp:Label>
+            </div>
+
+            <div id="loginDetails" class="col-md-4">
+            <asp:Label ID="Label3" runat="server" Text="Username:">
+                <asp:TextBox ID="username" runat="server"></asp:TextBox>
+            </asp:Label>
+            <asp:Label ID="Label4" runat="server" Text="Password:">
+                <asp:TextBox ID="password" runat="server"></asp:TextBox>
+            </asp:Label>
+            <asp:Label ID="Label5" runat="server" Text="Additional Info:">
+                <asp:TextBox ID="info" runat="server"></asp:TextBox>
+            </asp:Label>
+            <asp:Label ID="Label6" runat="server" Text="Email Address:">
+                <asp:TextBox ID="email" runat="server"></asp:TextBox>
+            </asp:Label>
+            </div>
+
+            <asp:Button ID="add" runat="server" Text="Add Login" />
+
         </div>
     </form>
 </body>
--- WebProg/AddLogin.aspx.designer.cs
+++ WebProg/AddLogin.aspx.designer.cs
@@ -29,5 +29,113 @@ namespace WebProg {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// websiteName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox websiteName;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// link control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox link;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// username control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox username;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// password control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox password;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// info control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox info;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// email control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+        
+        /// <summary>
+        /// add control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button add;
     }
 }
